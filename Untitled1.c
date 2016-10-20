3ec3b48283c3ba07473ac45f5267ce98b0f99664
aarush07 <rijalaarush7@gmail.com>
2016-10-20 11:55:06 +0545

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,27 +1,23 @@
-/*
-Student name: Aarush Baba Rijal
-Subject     :ProgrammingFundamentals
-Roll        :01
-Lab no.     :03
-program.    :To Calculate Compound Intrest
-*/
-
 #include<stdio.h>
 #include<conio.h>
-#include<math.h>
-        void main()
-            {
-            float p,t,r,ci=0;
+    void main()
+        {
+            int a,b;
+            printf("enter a number: \n");
 
-            printf("Enter the principle");
-            scanf("%f",&p);
-            printf("Enter the rate");
-            scanf("%f",&r);
-            printf("Enter the time");
-            scanf("%f",&t);
-            ci=p*(pow(1+r/100,t)-1);
-            printf("The compound intrest is: %f",ci);
-            getch();
-
-
-            }
+            scanf("%d",&a);
+                printf("Enter another number: \n");
+                scanf("%d",&b);
+                printf("THe use of Assignment Operator:\n");
+                printf("The use of += \n");
+                printf("%d",a+=b);
+                printf("The use of -= \n");
+                printf("%d",a-=b);
+                 printf("The use of *= \n");
+                printf("%d",a*=b);
+                 printf("The use of /= \n");
+                printf("%d",a/=b);
+                 printf("The use of %= \n");
+                printf("%d",a%=b);
+                getch();
+        }
