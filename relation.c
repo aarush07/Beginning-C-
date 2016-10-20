231bf041d4692fa486eced0191cb647deed18da3
aarush07 <rijalaarush7@gmail.com>
2016-10-20 12:11:25 +0545

    Add files via upload

---
+++ relation.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+    int main()
+            {
+                int a,b;
+                printf("enter a number: \n");
+
+
+        scanf("%d",&a);
+                printf("Enter another number: \n");
+                scanf("%d",&b);
+
+                printf("THe use of Relation Operator:\n");
+                printf("The use of == \n");
+                printf("%d \n",a==b);
+                printf("The use of > \n");
+                printf("%d\n",a>b);
+                 printf("The use of < \n");
+                printf("%d\n",a<b);
+                 printf("The use of != \n");
+                printf("%d\n",a!=b);
+                 printf("The use of >= \n");
+                printf("%d\n",a>=b);
+                printf("The use of <= \n");
+                printf("%d\n",a<=b);
+                getch();
+                }
