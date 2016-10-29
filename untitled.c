250e8a701274068d088c581a9700a82d23fa241f
aarush07 <rijalaarush7@gmail.com>
2016-10-29 10:41:39 +0545

    vowels/constant

---
+++ untitled.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//#inlclude <conio.h>
+			
+						
+					int main()
+					{
+						
+					char z;
+					printf("Enter a char: \n");
+					scanf("%c",&z);
+							
+					(z=='a'||'e' ||'i' || 'o' ||'u')? printf("vowel"): printf("consonant");
+					return 0;
+						}
+						
