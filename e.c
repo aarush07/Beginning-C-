68f46b6daf2abdae49a4512cb32dae652963c1d2
aarush07 <rijalaarush7@gmail.com>
2016-10-29 10:23:03 +0545

    sum,product,average,smallest and greatest

--- e.c
+++ e.c
@@ -1,14 +1,47 @@
-
- 	#include<stdio.h>
+	
+		#include<stdio.h>
 		
-	int main()
+		int main()
 		{
-			int a;
-				printf("This program uses terniary operator to check odd and even:\n");
-				printf("Enter a number:\n");
-				scanf("%d",&a);
-				(a%2==0) ? printf("The number is even \n "):printf("The number is odd \n");
-		
+				
+		int a,b,c,sum=0,average=0,greatest=0,smallest=0,product=0;
+	
+		printf("Enter three numbers: \n");
+		scanf("%d %d %d",&a,&b,&c);
+		sum=a+b+c;
+		product=a*b*c;
+		average=(a+b+c)/3;
+		if (a>=b && a>=c)
+		{
+			greatest=a;
+		}
+		else if (b>=a && b>=c)
+		{
+			greatest=b;
+		}
+		else
+		{
+			greatest=c;
+		}
+	if (a<=b && a<=c)
+		{
+			smallest=a;
+		}
+		else if (b<=a && b<=c)
+		{
+			smallest=b;
+		}
+		else
+		{
+			smallest=c;
+		}
+	
+	printf("The greatest is %d \n",greatest);
+	printf("The sum of three number is %d \n",sum);
+	printf("The product of three number is %d \n",product);
+	printf("The average of three number is %d \n",average);
+	printf("The smallest of three number is %d \n",smallest);
 
-			return 0;	
+
+		return 0;			
 		}
