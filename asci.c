fe4de5b59c9b199e21de01233e4073df7f3ac1af
aarush07 <rijalaarush7@gmail.com>
2016-10-29 10:16:14 +0545

    corresponding asci value

---
+++ asci.c
@@ -0,0 +1,15 @@
+						
+
+
+
+	#include<stdio.h>
+	//#include<conio.h>
+		
+		int main()		
+			{
+				char a;
+				printf("Enter a character");
+				scanf("%c",&a);
+				printf("%d",a);
+				
+			}
