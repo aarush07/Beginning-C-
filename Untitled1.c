cca3f340420fb874c2fabcbfd6145f4ec71feb44
aarush07 <rijalaarush7@gmail.com>
2016-10-29 09:53:20 +0545

    assignment operator

--- Untitled1.c
+++ Untitled1.c
@@ -8,7 +8,7 @@
             scanf("%d",&a);
                 printf("Enter another number: \n");
                 scanf("%d",&b);
-                printf("THe use of Assignment Operator:\n");
+                printf("The use of Assignment Operator:\n");
                 printf("The use of += \n");
                 printf("%d",a+=b);
                 printf("The use of -= \n");
