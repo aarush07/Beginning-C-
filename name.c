7b486b44efb41cfe06b24350d3d7d66fd670a624
aarush07 <rijalaarush7@gmail.com>
2016-10-29 22:46:53 +0545

    display string

---
+++ name.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+ 
+int main()
+{
+    char array[10];
+ 
+    printf("Enter a string\n");
+    scanf("%s", array);
+ 
+    printf("You entered the string %s\n",array);
+    return 0;
+}
